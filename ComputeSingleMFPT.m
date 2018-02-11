0cca640101eb97afa23585a974f12b11f4f75e7a
Andrea Ramazzina <andrea_ramazzina@hotmail.it>
2018-02-11 16:49:45 +0100

    Update ComputeSingleMFPT.m

--- ComputeSingleMFPT.m
+++ ComputeSingleMFPT.m
@@ -1,19 +1,21 @@
+% Compute the Mean First Passage Time from a node i to a node j, given a Network with adjacency matrix A
+% The variables are named according to the paper “Mean first-passage time for random walks on undirected networks"
+
 function T = ComputeSingleMFPT(A, i,j)
 
     N = size(A,2);
     degree = sum(A);
     K = sum(degree);
     D = diag(degree);
-    
     S = D^(-1/2) * A * D^(-1/2) ;
     
+    %Extract eigenvectors and eigenvalues, sorting them in decrescent order:
     [psi,lambda] = eig(S);
-    
     lambda = rot90(lambda,2);
     lambda = diag(lambda);
     psi = fliplr(psi);
 
-
+    %Compute the MFPT:
     summa = 0;
     for k = 2:N
         firstTerm = 1/(1-lambda(k));
@@ -22,6 +24,5 @@ function T = ComputeSingleMFPT(A, i,j)
         
         summa = summa + firstTerm*(secondTerm-thirdTerm);
     end
-        
     T = K * summa;
 end
