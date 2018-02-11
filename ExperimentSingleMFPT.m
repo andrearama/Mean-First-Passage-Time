9f06269022e7557e760f6d217c9d89e252efaeb2
Andrea Ramazzina <andrea_ramazzina@hotmail.it>
2018-02-11 16:58:56 +0100

    Update ExperimentSingleMFPT.m

--- ExperimentSingleMFPT.m
+++ ExperimentSingleMFPT.m
@@ -1,8 +1,13 @@
-function ET = ExperimentSingleMFPT(A,startP,endP,nIterations)
-    
-    ET = zeros(nIterations,1);
-    for iter=1:nIterations
+% Estimate the MFPT between two nodes, given a Network with adjacency matrix A
 
+function T = ExperimentSingleMFPT(A,startP,endP,nIterations)
+    
+    %Initialize the vector containing the number of steps needed to reach the target in each simulation:
+    ET = zeros(nIterations,1);
+    
+    for iter=1:nIterations
+    
+        % Simulate Random Walk throughout the network until the end point is reached:
         actualP = startP;
             while actualP ~= endP
                 possibleP = find(A(actualP,:) ==1);
@@ -10,7 +15,10 @@ function ET = ExperimentSingleMFPT(A,startP,endP,nIterations)
                 actualP = possibleP(index);
                 ET(iter) = ET(iter)+1;
             end 
+            
     end
-    ET= mean(ET);
+    
+    %Estimate the MFPT as the average of the experimental results:
+    T= mean(ET);
     
 end
